48a0d898186a31ea9568b99b43849c9dc89f02bd
Nonbinary-A <4thson33@gmail.com>
2020-07-21 13:59:21 +0300

    sort button event + functon

--- src/main.js
+++ src/main.js
@@ -34,8 +34,40 @@ divDate.className = "todoCreatedAt";
 let date = new Date().toISOString().slice(0, 19).replace('T', ' ');
 divDate.textContent = date;
 
-// appending childs
+// appending childs to container
 list.appendChild(todoContainer);
 todoContainer.appendChild(divPriority);
 todoContainer.appendChild(divDate);
 todoContainer.appendChild(textTodo);
+
+// counting childs for counter
+counter = document.getElementById("list").childElementCount;
+document.getElementById("counter").innerHTML = counter;
+}
+
+// sort button event
+sortButton.onclick = function(){
+    let sList, i, switching, b, shouldSwitch;
+    sList = document.getElementById("list");
+    // The loop continues till there is no switching 
+    switching = true;
+    while (switching) {
+        switching = false;
+        b = sList.getElementsByClassName("todoContainer");
+        // looping through the list items
+        for (i = 0; i < (b.length - 1); i++) {
+            shouldSwitch = false;
+            // checking which value is bigger
+            if((b[i].innerHTML) < (b[i+1].innerHTML)){
+                // if it's not bigger, mark as a switch and break the loop
+                shouldSwitch = true;
+                break;
+            }
+        }
+    if (shouldSwitch) {
+        // if the switched had happend, make it and mark it as true
+        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        switching = true;
+           }    
+        }
+    }
