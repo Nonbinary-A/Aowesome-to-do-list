9c9a67bc9e39af0027c715e68fdfc20dcad8a1ed
Nonbinary-A <4thson33@gmail.com>
2020-07-21 13:47:06 +0300

    Declare todo container and childs

--- src/main.js
+++ src/main.js
@@ -11,3 +11,22 @@ const sortButton = document.getElementById("sortButton");
 button.onclick = function(){
     let todoItem = input.value;
     input.value = '';
+// div container 
+const todoContainer = document.createElement("div");
+todoContainer.className = "todoContainer";
+// div textTodo object
+const textTodo = document.createElement("div");
+textTodo.className ="todoText"
+textTodo.textContent = todoItem;
+// div priority
+const divPriority = document.createElement("div");
+divPriority.className = "todoPriority";
+//value of priority
+const priority = document.getElementById("prioritySelector").value;
+divPriority.textContent = priority;
+//div date
+const divDate = document.createElement("div");
+divDate.className = "todoCreatedAt";
+//get the sql date
+let date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+divDate.textContent = date;
