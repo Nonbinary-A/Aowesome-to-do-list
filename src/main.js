98a50f495edb875a7bc37d3e948af243b7ebccf3
Nonbinary-A <4thson33@gmail.com>
2020-07-21 13:32:05 +0300

    defining elements, counter and sort button

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// Definning elements
+const list = document.getElementById("list");
+const input = document.getElementById("textInput");
+const button = document.getElementById("addButton");
+// Defining counter
+let counter = 0;
+//Defining sort button
+const sortButton = document.getElementById("sortButton");
+
